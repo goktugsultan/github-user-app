d3b10d7f23b27eda1b267725e905b8012ee0b91e
Goktug SULTAN <goktugsultan@gmail.com>
2019-11-08 20:31:48 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from 'react';
+import { Form, Card, Image, Icon, Message } from 'semantic-ui-react'
+import './App.css';
+
+function App() {
+  const [name, setName] = useState('')
+  const [userName, setUsername] = useState('')
+  const [followers, setFollowers] = useState('')
+  const [following, setFollowing] = useState('')
+  const [repos, setRepos] = useState('')
+  const [avatar, setAvatar] = useState('')
+  const [userInput, setUserInput] = useState('')
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/example")
+    .then(res => res.json())
+    .then(data => {
+      setData(data)
+    })
+  },[])
+
+  const setData = ({
+    name, 
+    login, 
+    followers, 
+    following, 
+    public_repos, 
+    avatar_url
+  }) => {
+    setName(name)
+    setUsername(login)
+    setFollowers(followers)
+    setFollowing(following)
+    setRepos(public_repos)
+    setAvatar(avatar_url)
+  }
+
+  const handleSearch = (e) => {
+    setUserInput(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    fetch(`https://api.github.com/users/${userInput}`)
+    .then(res => res.json())
+    .then(data => {
+      if(data.message){
+        setError(data.message)
+      }
+      else{
+        setData(data)
+        setError(null)
+      }
+    })
+  }
+
+  return (
+    <div >
+      <div className="navBar">Github Search</div>
+      <div className="search">
+        <Form onSubmit={handleSubmit} >
+          <Form.Group>
+            <Form.Input
+              placeholder="Github user"
+              name="github user"
+              onChange={handleSearch}
+            />
+            <Form.Button content="Search" negative />
+          </Form.Group>
+        </Form>
+      </div>
+      {error ? 
+        (
+        <div className="errorMessage">
+        <Message >
+          
+          <Message.Header>{error}</Message.Header>
+          <p>
+           {error}
+          </p>
+          
+        </Message>
+        </div>
+       ) : (
+        <div className="card">
+          <Card>
+              <Image src={avatar} wrapped ui={false} />
+              <Card.Content>
+                  <Card.Header>{name}</Card.Header>
+                  <Card.Header>{userName}</Card.Header>
+              </Card.Content>
+              <Card.Content extra>
+                <a>
+                  <Icon name='user' />
+                  {followers} Followers
+                </a>
+              </Card.Content>
+              <Card.Content extra>
+                <a>
+                  <Icon name='file' />
+                  {repos} Repos
+                </a>
+              </Card.Content>
+              <Card.Content extra>
+                <a>
+                  <Icon name='user' />
+                  {following} Following
+                </a>
+              </Card.Content>
+          </Card>
+      </div>
+   )}
+      
+    </div>
+  );
+}
+
+export default App;
